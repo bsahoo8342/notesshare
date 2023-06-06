c87627d698cd5e5e7b42fd8f53caf8be2df1af17
bsahoo8342 <70257740+bsahoo8342@users.noreply.github.com>
2023-06-06 17:07:24 +0530

    card.php

--- card.php
+++ card.php
@@ -1,34 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
+    <head>
     <link rel="stylesheet" href="css/style4.css">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Document</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-
-</head>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    </head>
 
 <body>
 
   <div class="card" style="width: 18rem;">
     <div class="card-body">
-      <h5 class="card-title">BSc IST</h5>
-      <h6 class="card-subtitle mb-2 text-muted">Notes for BSc IST Students</h6>
-      <a href="#" class="card-link">Click Here</a>
-    </div>
+        <h5 class="card-title">BSc IST</h5>
+        <h6 class="card-subtitle mb-2 text-muted">Notes for BSc IST Students</h6>
+        <a href="#" class="card-link">Click Here</a></div>
   </div>
 
   <div class="card" style="width: 18rem;">
     <div class="card-body">
       <h5 class="card-title">BSc ITM</h5>
       <h6 class="card-subtitle mb-2 text-muted">Notes for BSc ITM Students</h6>
-      <a href="#" class="card-link">Click Here</a>
-    </div>
+      <a href="#" class="card-link">Click Here</a></div>
   </div>
 
   <div class="card" style="width: 18rem;">
@@ -36,8 +33,7 @@
       <h5 class="card-title">BSc Computer Science</h5>
       <h6 class="card-subtitle mb-2 text-muted">Notes for BSc Computer Science Students</h6>
       <a href="#" class="card-link">Click Here</a><br>
-      <span class="span1">Notes not ready</span>
-    </div>
+      <span class="span1">Notes not ready</span></div>
   </div>
 
   <div class="card" style="width: 18rem;">
@@ -45,8 +41,7 @@
       <h5 class="card-title">BSc Zoology</h5>
       <h6 class="card-subtitle mb-2 text-muted">Notes for BSc Zoology Students</h6>
       <a href="#" class="card-link">Click Here</a><br>
-      <span class="span1">Notes not ready</span>
-    </div>
+      <span class="span1">Notes not ready</span></div>
   </div>
 
   <div class="card" style="width: 18rem;">
@@ -54,8 +49,7 @@
       <h5 class="card-title">BSc Botany</h5>
       <h6 class="card-subtitle mb-2 text-muted">Notes for BSc Botany Students</h6>
       <a href="#" class="card-link">Click Here</a><br>
-      <span class="span1">Notes not ready</span>
-    </div>
+      <span class="span1">Notes not ready</span></div>
   </div>
 
   <div class="card" style="width: 18rem;">
@@ -63,8 +57,7 @@
       <h5 class="card-title">BSc Chemistry</h5>
       <h6 class="card-subtitle mb-2 text-muted">Notes for BSc Chemistry Students</h6>
       <a href="#" class="card-link">Click Here</a><br>
-      <span class="span1">Notes not ready</span>
-    </div>
+      <span class="span1">Notes not ready</span></div>
   </div>
 
   <div class="card" style="width: 18rem;">
@@ -72,13 +65,8 @@
       <h5 class="card-title">BSc Mathematics</h5>
       <h6 class="card-subtitle mb-2 text-muted">Notes for BSc Mathematics Students</h6>
       <a href="#" class="card-link">Click Here</a><br>
-      <span class="span1">Notes not ready</span>
-    </div>
+      <span class="span1">Notes not ready</span></div>
   </div>
-
-
-<!-- <button onClick="window.location.href='index.php';">Home</button> -->
-
 </body>
 
 </html>
